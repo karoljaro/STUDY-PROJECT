98a5d4946df8729c0fa0f04cf4d777162204ba37
karoljaro <111643413+karoljaro@users.noreply.github.com>
2025-06-04 22:06:36 +0200

    Task6: Complete XML parsing functionality - XML to JSON/YAML conversion working

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,1 +1,7 @@
 """Parsers package for data format conversion."""
+
+from .json_parser import JSONParser
+from .yaml_parser import YAMLParser  
+from .xml_parser import XMLParser
+
+__all__ = ['JSONParser', 'YAMLParser', 'XMLParser']
